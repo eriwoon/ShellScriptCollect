3da2941a0f74a5960c622917104709c6229cc7c1
eric.xiaozhen <eric.xiaozhen@foxmail.com>
2014-06-05 19:29:47 +0200

    Update Collect.sh

--- Collect.sh
+++ Collect.sh
@@ -38,3 +38,128 @@ do
     "http://10.88.150.9:7782//services/CBSInterfaceBusinessMgrService" \
     > /dev/null
 done
+
+########################################################################################
+
+#自动远程登录主机的关键部分
+for ip in $IP
+do
+        echo %ip
+        expect -c "
+                spawn ssh -o StrictHostKeyChecking=no ${username}@$ip \"${command}\"
+                expect *assword:*;
+                send -- $password\r;
+                interact;"
+done
+
+#从stdin读数据的脚本
+#! /bin/sh
+
+i=1
+
+while read line
+do
+        #WebLog
+        if [ "x`echo $line | grep weblog`" != "x" ]; then
+                echo $line | cut -d'|' -f1 | cut -d':' -f2 | xargs echo -n
+
+        #mon
+        elif [ "x`echo $line | grep omon`" != "x" ]; then
+                echo $line | cut -d'|' -f2 | xargs echo -n
+
+        #mgr
+        elif [ "x`echo $line | grep omgr`" != "x" ]; then
+                echo $line | cut -d'|' -f3 | xargs echo -n
+
+        #data
+        elif [ "x`echo $line | grep odata`" != "x" ]; then
+                echo $line | cut -d'|' -f3 | xargs echo -n
+
+        #SMSSend
+        elif [ "x`echo $line | grep cbpsmssend`" != "x" ]; then
+                echo $line | cut -d'|' -f1 | cut -d':' -f2 | xargs echo -n
+
+        #expireddata
+        elif [ "x`echo $line | grep expireddata`" != "x" ]; then
+                echo $line | cut -d'|' -f1 | cut -d':' -f2 | xargs echo -n
+
+        fi
+        echo ':'$line
+
+done
+
+##################################################################################
+#关于时间格式修改的脚本
+#!/bin/sh
+
+CONF_FILE="./xz_vsr_getinfo.cfg"
+
+for pair in `cat $CONF_FILE | grep '^[^#].*'`
+do
+    #echo $pair
+    pair_name=`echo "$pair" | awk -F"=" '{print $1}'`
+    pair_value=`echo "$pair" | awk -F"=" '{print $2}'`
+    if [ "x$pair_name" != "x" -a  "x$pair_value" != "x" ]; then
+        eval $pair_name=$pair_value
+    fi
+done
+
+#Get the time information
+if [ "x$1" == "x" ] ; then
+    tm_date=`date -d $tm_date +%Y%m%d`
+else
+    tm_date=`date -d $1 +%Y%m%d`
+fi
+#tm_tomorrow=`date -d "$tm_date 1 days" +%Y%m%d`
+
+##initialise variables
+out_filename="ocs_info_${tm_date}.csv"
+lis_offerid=`echo $lis_offerid | sed 's/,/ /g'`
+
+echo -n "datetime${delimiter}Customer${delimiter}subscriber${delimiter}FUPuser" > $out_filename
+if [ "x$lis_offerid" != "x" ]; then
+    for offerid in $lis_offerid
+    do 
+        echo -n "${delimiter}$offerid" >> $out_filename
+    done
+fi
+echo "">> $out_filename
+
+#get the second
+#tm_date_sec=`date -d $tm_date +%s`
+#tm_tomorrow_sec=`date -d $tm_tomorrow +%s`
+#tm_tomorrow_sec=`expr $tm_tomorrow_sec - 1 `
+
+#tm_cur=`expr $tm_date_sec - 1`
+
+#while [ $tm_cur -lt $tm_tomorrow_sec ]
+#do
+    tm_begin=${tm_date}235959
+    #tm_cur=`expr $tm_cur + 3600` 
+    #tm_begin=`date -d @$tm_cur +%Y%m%d%H%M%S`
+    #echo -n "$tm_begin  "; date
+    echo -n $tm_begin >> $out_filename
+    
+    #A
+    re_sql=`mdsql -c "select count(*) from A where APPLYTIME < '$tm_begin' and EXPIRETIME > '$tm_begin';" | head -3 | tail -1 | awk '{print $1}'`
+    echo -n "${delimiter}$re_sql" >> $out_filename
+    
+    #B
+    re_sql=`mdsql -c "select count(*) from B where APPLYTIME < '$tm_begin' and EXPIRETIME > '$tm_begin';" | head -3 | tail -1 | awk '{print $1}'`
+    echo -n "${delimiter}$re_sql"  >> $out_filename
+
+    re_sql=`mdsql -c "select count(*) from C where (FUPSTATUS = '24' or FUPSTATUS = '25' or FUPSTATUS = '26') and APPLYTIME < '$tm_begin' and EXPIRETIME > '$tm_begin';" | head -3 | tail -1 | awk '{print $1}'`
+    echo -n "${delimiter}$re_sql" >> $out_filename
+    
+    if [ "x$lis_offerid" != "x" ]; then
+        for offerid in $lis_offerid
+        do 
+            re_sql=`mdsql -c "select count(*) from D where PRODUCTOFFERKEY = $offerid and APPLYTIME < '$tm_begin' and EXPIRETIME > '$tm_begin';" | head -3 | tail -1 | awk '{print $1}'`
+            echo -n "${delimiter}$re_sql" >> $out_filename
+        done
+    fi
+    
+    echo "" >> $out_filename
+    
+#done
+#cat $out_filename
