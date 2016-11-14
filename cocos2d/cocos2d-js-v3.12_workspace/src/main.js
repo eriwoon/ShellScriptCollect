6520ebe2033204b9c1ccbdef12b836cf9b06e48f
eric.xiaozhen <eric.xiaozhen@foxmail.com>
2016-11-14 23:14:10 +0800

    src/main.js

--- cocos2d/cocos2d-js-v3.12_workspace/src/main.js
+++ cocos2d/cocos2d-js-v3.12_workspace/src/main.js
@@ -1,26 +1,29 @@
 window.onload = function(){
-    cc.game.onStart = function(){
-        //load resources
-        cc.LoaderScene.preload(["HelloWorld.png"], function () {
-          var MyScene = cc.Scene.extend({
-            onEnter:function () {
-              this._super();
-              var size = cc.director.getWinSize();
-              var sprite = cc.Sprite.create("HelloWorld.png");
-              sprite.setPosition(size.width / 2, size.height / 2);
-              sprite.setScale(0.8);
-              this.addChild(sprite, 0);
 
-              var label = cc.LabelTTF.create("Hello World", "Arial", 40);
-              label.setPosition(size.width / 2, size.height / 2);
-              this.addChild(label, 1);
-            }
-          });
-          //cc.director.runScene(new MyScene());
-          cc.director.runScene(new startScene());
-        }, this);
-    };
-    cc.game.run("gameCanvas");
+
+
+  cc.game.onStart = function(){
+    //load resources
+    cc.LoaderScene.preload(["HelloWorld.png"], function () {
+      var MyScene = cc.Scene.extend({
+        onEnter:function () {
+          this._super();
+          var size = cc.director.getWinSize();
+          var sprite = cc.Sprite.create("HelloWorld.png");
+          sprite.setPosition(size.width / 2, size.height / 2);
+          sprite.setScale(0.8);
+          this.addChild(sprite, 0);
+
+          var label = cc.LabelTTF.create("Hello World", "Arial", 40);
+          label.setPosition(size.width / 2, size.height / 2);
+          this.addChild(label, 1);
+        }
+      });
+      //cc.director.runScene(new MyScene());
+      cc.director.runScene(new startScene());
+    }, this);
+  };
+  cc.game.run("gameCanvas");
 };
 
 var startLayer = cc.Layer.extend({
@@ -69,3 +72,30 @@ var startScene = cc.Scene.extend({
   }
 });
 
+
+var getWinSize = function(){
+  var winSize = null;
+  // get window width
+  if (window.innerWidth)
+  winSize.winWidth = window.innerWidth;
+  else if ((document.body) && (document.body.clientWidth))
+  winSize.winWidth = document.body.clientWidth;
+  // get window height
+  if (window.innerHeight)
+  winSize.winHeight = window.innerHeight;
+  else if ((document.body) && (document.body.clientHeight))
+  winSize.winHeight = document.body.clientHeight;
+  // get window size by deep dive body inside Document
+  if (document.documentElement && document.documentElement.clientHeight && document.documentElement.clientWidth)
+  {
+  winSize.winHeight = document.documentElement.clientHeight;
+  winSize.winWidth = document.documentElement.clientWidth;
+
+  return winSize;
+};
+
+var resizeCanvas = function(){
+  var winSize = getWinSize();
+  var canvas = document.getElementByName("canvas")[0];
+  canvas.
+};
